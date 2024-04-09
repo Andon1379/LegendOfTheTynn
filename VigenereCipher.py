c402b6319f5af30bb6608aeb629bda5a48ba54e9
Artemis Calia-Bogan <afcb1379@gmail.com>
2024-04-09 16:48:11 -0400

    added Vigenere cipher encrypter / decrypter

---
+++ VigenereCipher.py
@@ -0,0 +1,24 @@
+unicode_offset = ord('a')
+
+def encrypt(input:str, key:str):
+    input = list(input.casefold())
+    key = list(key.casefold())
+    key_len = len(key)
+    ans = ""
+    
+    for i in range(len(input)):
+        # print(i, input[i], key[i%key_len]) 
+        ans += chr((ord(input[i]) + ord(key[i%key_len]) - (unicode_offset * 2))%26 + unicode_offset)
+        
+    return ans
+
+def decrypt(input:str, key:str):
+    input = list(input.casefold())
+    key = list(key.casefold())
+    key_len = len(key)
+    ans = ""
+    
+    for i in range(len(input)):
+        ans += chr((ord(input[i]) - ord(key[i%key_len]))%26 + unicode_offset)
+        
+    return ans
