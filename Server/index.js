7f939c4b85ea7e74bd2a72ec71781ae7b25ae76f
Andon1379 <afcb1379@gmail.com>
2024-04-24 22:04:40 -0400

    fixed pashash function on /forum/pass

--- Server/index.js
+++ Server/index.js
@@ -474,12 +474,12 @@ app.post('/forum/verify', (req, res) => {
 app.post('/forum/pass', (req, res) => {
   console.log(req.body)
 
-  InlineForumPass.addAttempt(new passAttempt(req.body.passHash))
+  InlineForumPass.addAttempt(new passAttempt(req.body.passhash))
   writeStatsFile('./inlineForumStats.json', InlineForumPass)
   
   postPasses.forEach((post, index) => {
     // console.log(post, req.body, req.body.passHash == post.passHash)
-    if((post.passHash == 0 || req.body.passHash == post.passHash) && req.body.index == index) {
+    if((post.passhash == 0 || req.body.passhash == post.passhash) && req.body.index == index) {
       res.send({index:index, destination: post.destination});
     } else {res.end()}
   })
